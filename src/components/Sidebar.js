c3b9e26e0378c194d9d88898e296eb582cb8c762
centek134 <centek134@wp.pl>
2021-08-02 10:39:21 +0200

    Added new position in Sidebar now we can add another channels

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,52 +1,52 @@
-import React ,{ useState, useEffect } from 'react';
-import styled from 'styled-components';
-import {db} from '../utility/firebase.js';
-import RoomItem from './RoomItem.js';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { db } from "../utility/firebase.js";
+import RoomItem from "./RoomItem.js";
 
 const SidebarCont = styled.div`
-    flex: 0.15;
-    min-width: 200px;
-    background-color: #8A8AFF;
-    height: 100%;
-    color: white;
-    display: flex;
-    flex-flow: column;
-    & > h3{
-        text-align: center;
-        margin: 10px 0;
-        padding: 10px 0;
-        border-bottom: 2px solid #ffffff;
-    }
+  flex: 0.15;
+  min-width: 200px;
+  background-color: #8a8aff;
+  min-height: 100vh;
+  height: 100%;
+  color: white;
+  display: flex;
+  flex-flow: column;
+  & > h3 {
+    text-align: center;
+    margin: 10px 0;
+    padding: 10px 0;
+    border-bottom: 2px solid #ffffff;
+  }
 `;
 
 const Sidebar = () => {
+  const [rooms, setRooms] = useState([]);
 
-    const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) => {
+      setRooms(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+        }))
+      );
+    });
+  }, []);
 
-    useEffect( () => {
-       db.collection("rooms").onSnapshot( snapshot => {
-            setRooms(
-                snapshot.docs.map(doc => ({
-                    id: doc.id,
-                    name: doc.data().name
-                }))
-            )
-        });
-    }, [])
+  return (
+    <SidebarCont>
+      <div>
+        <h2>Tomasz Osuch</h2>
+      </div>
 
-    return (
-        <SidebarCont>
-            <div>
-                <h2>Tomasz Osuch</h2>
-            </div>
-
-
-            <h3>Channels</h3>
-            {rooms.map( room => (
-                <RoomItem key={room.id} title = {room.name}></RoomItem>
-            ))}
-        </SidebarCont>
-    );
+      <h3>Channels</h3>
+      <RoomItem addRoomOption={true} title="Add Channel"></RoomItem>
+      {rooms.map((room) => (
+        <RoomItem key={room.id} title={room.name} id={room.id}></RoomItem>
+      ))}
+    </SidebarCont>
+  );
 };
 
 export default Sidebar;
