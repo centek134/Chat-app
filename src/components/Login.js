74f59844c017ba4bed6b533d9a2532175469d2d7
centek134 <centek134@wp.pl>
2021-09-07 19:12:26 +0200

    Setting google authorization, styled login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
+import { auth, provider } from '../utility/firebase';
 
 
 const LoginWindow = styled.div`
@@ -9,7 +10,7 @@ const LoginWindow = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #F7F7F7;
+    background-color:#069CFF ;//#F7F7F7;
     & > div {
         display: flex;
         justify-content: center;
@@ -17,7 +18,8 @@ const LoginWindow = styled.div`
         flex-direction: column;
         min-height: 350px;
         border-radius: 15px;
-        background-color: #ffffff;
+        background-color: #069CFF;//#ffffff;
+        color: #ffffff;
         max-width: 500px;
         & > h1{
             font-size: 60px;
@@ -28,20 +30,41 @@ const LoginWindow = styled.div`
         }
         & > button{
             font-size: 20px;
+            font-weight: 600;
             padding: 15px 5px;
             width: 200px;
+            border: 3px solid #ffffff;
+            background-color: #069CFF;
+            color: #ffffff;
+            cursor: pointer;
+            transition: 400ms;
+            &:hover{
+                background-color: #ffffff;
+                color: #069CFF;
+                border: 3px solid #069CFF;
+            }
         }
     } 
 `;
 
 const Login = (props) => {
+
+
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+        .then((result) => {
+            console.log("USER INFO >>>>", result);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
     return (
     <LoginWindow>
         <div>
 
         <h1>Chat App</h1>
         <h3>Do you like typing with people? Don't let this opportunity slip away! Let's join us and start making friends.</h3>
-        <button>Sign in with Google</button>
+        <button onClick ={signIn}>Sign in with Google</button>
         </div>
     </LoginWindow>
             
